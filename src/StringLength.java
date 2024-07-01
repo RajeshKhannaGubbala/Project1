6b2c5180252cec3cd99382374ad930c5f263fa0a
RajeshKhannaGubbala <157958140+RajeshKhannaGubbala@users.noreply.github.com>
2024-07-01 12:53:10 +0530

    Code changes

---
+++ src/StringLength.java
@@ -0,0 +1,19 @@
+
+public class StringLength {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str = "RajeshKhanna";
+		char[] ch = str.toCharArray();
+		
+		for(int i=0;i<str.length();i++) {
+		
+		//for(int i=str.length()-1;i>=0;i--) {
+			System.out.println(ch[i]);
+			
+		}
+
+	}
+
+}
